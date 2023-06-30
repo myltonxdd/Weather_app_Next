97e7673a283e203a928364d5264618fa54176346
Milton Procel M <miltonprocel@hotmail.com>
2023-06-29 23:48:09 -0500

    barra izquierda

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,9 @@
 "use client"
 import CardImg from "@/components/CardImg"
+import BarLeft from "@/components/BarLeft"
 import Button from 'react-bootstrap/Button';
 import Image from "next/image";
+
 /* import { useState, useEffect } from "react" */
 
 /* https://api.openweathermap.org/data/2.5/forecast?id=3652462&appid=bc07aa562eedf9f9d4bb3b8aa994f8b7 */
@@ -10,7 +12,7 @@ const location = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" 
 <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
 </svg>
 
-const iconTarg = <svg width="13" height="13" viewBox="0 0 24 25" fill="currentColor" className="d-flex bi bi-geo-alt-fill h-100 w-100" xmlns="http://www.w3.org/2000/svg"><path d="M11 2a1 1 0 0 1 2 0v2.062A8.004 8.004 0 0 1 19.938 11H22a1 1 0 0 1 0 2h-2.062A8.004 8.004 0 0 1 13 19.938V22a1 1 0 0 1-2 0v-2.062A8.004 8.004 0 0 1 4.062 13H2a1 1 0 0 1 0-2h2.062A8.004 8.004 0 0 1 11 4.062V2zm7 10a6 6 0 1 0-12 0 6 6 0 0 0 12 0zm-3 0a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" fill="#000000"/></svg>
+const iconTarg = <svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" fill="white" viewBox="0 -960 960 960" ><path d="M450-42v-75q-137-14-228-105T117-450H42v-60h75q14-137 105-228t228-105v-75h60v75q137 14 228 105t105 228h75v60h-75q-14 137-105 228T510-117v75h-60Zm30-134q125 0 214.5-89.5T784-480q0-125-89.5-214.5T480-784q-125 0-214.5 89.5T176-480q0 125 89.5 214.5T480-176Zm0-154q-63 0-106.5-43.5T330-480q0-63 43.5-106.5T480-630q63 0 106.5 43.5T630-480q0 63-43.5 106.5T480-330Z"/></svg>
 
 async function getData(){
   const parameter = await fetch("https://api.openweathermap.org/data/2.5/weather?q=quito&appid=bc07aa562eedf9f9d4bb3b8aa994f8b7")
@@ -29,9 +31,13 @@ export default async function Home() {
   return (
     <div className="d-flex display-raw flex-wrap" style={{backgroundColor:"#100e1d"}}>
       
+      {/*////////////// izquierda BUSQUEDA/////////// */}
       
+      <BarLeft/>
+
+
       {/*///////////////Izquierda/////////////////// */}
-      <div className="d-flex flex-column align-items-center text-white-50 w-100 py-4" style={{backgroundColor:"#1e213a", maxWidth:"28rem"}}>
+      <div className="d-flex flex-column align-items-center text-white-50 w-100 py-4" style={{backgroundColor:"#1e213a", maxWidth:"26rem"}}>
 
         <div className="d-flex flex-raw justify-content-between w-100">
           <Button variant="secondary" className="mx-2">Search for places</Button>{' '}
@@ -42,23 +48,19 @@ export default async function Home() {
           
           
         </div>
-        <div className="d-flex flex-column gap-3">
-          <h1 style={{fontSize:"60px"}}>15°C</h1>
-          <h2 >Shower</h2>
-          <div>Today - Fri, 5 jun</div>
-          <div>
-            {location} Helsinky
-          </div>
-        </div>
+        <div className="d-flex flex-column align-items-center gap-3 pb-1">
+          <h1 style={{fontSize:"15vh"}}>15°C</h1>
+          <h2 className="fs-2">Shower</h2>
+          <div className="fs-3">Today - Fri, 5 jun</div>
+          <div className="fs-3">
+            {location} Helsinky</div>
+        </div >
         
         
       
       </div>
 
-      {/*////////////// izquierda BUSQUEDA/////////// */}
-      {/* {<div >
-        <h1>Hol</h1>
-      </div>} */}
+      
 
       {/* ///////////// DERECHA //////////////// */}
 
---
+++ components/BarLeft.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import CloseButton from 'react-bootstrap/CloseButton';
+
+/////ICONOS///////
+
+const equis = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
+<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+</svg>
+
+const lupa = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#616475" className="bi bi-search" viewBox="0 0 16 16">
+<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+</svg>
+
+const arrow = <svg xmlns="http://www.w3.org/2000/svg" fill='#616475' width="30" height="30" viewBox="0 -960 960 960"><path d="m304-82-56-57 343-343-343-343 56-57 400 400L304-82Z"/></svg>
+
+
+function BarLeft() {
+  return (
+    <div className='d-flex display-raw flex-wrap position-fixed' style={{width:"100vh", height:"100vh"}}>
+      <div className='d-flex flex-column align-items-center gap-4 w-100' style={{backgroundColor:"#1e213a", maxWidth:"26rem"}}>
+        <div className='my-2 w-75 d-flex flex-row-reverse'><CloseButton variant='white'/>;</div>
+        <div className='d-flex gap-1'>
+          <div className='d-flex align-items-center gap-1 px-2' style={{border:"1px solid #616475"}}>
+            {lupa}
+            <input style={{backgroundColor:"#1e213a"}} className='border-0' placeholder='Search location' type="text" />
+          </div>
+          <button className='text-white p-1 border-0' style={{backgroundColor:"#3c47e9"}}>Search</button>
+        </div>
+        <div className='d-flex flex-column w-75 my-4 gap-4'>
+          <button className='text-white border-0 d-flex justify-content-between' style={{backgroundColor:"#1e213a"}}>London {arrow} </button>
+          <button className='text-white border-0 d-flex justify-content-between' style={{backgroundColor:"#1e213a"}}>Barcelona {arrow}</button>
+          <button className='text-white border-0 d-flex justify-content-between' style={{backgroundColor:"#1e213a"}}>Long Beach {arrow}</button>
+        </div>
+      </div>
+      <div >
+        
+      </div>
+      
+    </div>
+  )
+}
+
+export default BarLeft
